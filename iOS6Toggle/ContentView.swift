8228bf28a9a9daa00a4da910a2dd3dcdcb4496a7
Elvin Hu <elvin_hu@icloud.com>
2020-02-04 10:45:19 -0500

    Initial Commit

---
+++ iOS6Toggle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS6Toggle
+//
+//  Created by Elvin Hu on 2/4/20.
+//  Copyright © 2020 Elvin Hu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
